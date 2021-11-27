e5897a04d49a0f8a4aabed49b7f9237c4280243f
goldipl <mgodlewski92@gmail.com>
2021-11-27 13:30:42 +0100

    handle Output

--- src/comp/WeatherOutput.js
+++ src/comp/WeatherOutput.js
@@ -2,14 +2,28 @@ import React from 'react';
 
 const WeatherOutput = (props) => {
 
-  const { cityName, cityTemp, cityPressure, cityWind } = props.weather;
+  const { cityErr, cityName, cityTemp, cityPressure, cityWind } = props.weather;
+  let container = null;
+
+
+  if (cityName && !cityErr) {
+    container = (
+      <>
+        <div clasName="resultContainer">
+          <div><strong>City: </strong>{String(cityName)}</div>
+          <div><strong>Temp: </strong>{`${String(cityTemp)} *C`}</div>
+          <div><strong>Pressure: </strong>{`${String(cityPressure)} hPa`}</div>
+          <div><strong>Wind: </strong>{`${String(cityWind)} m/s`}</div>
+        </div>
+      </>
+    )
+  }
+
+
   return (
-    <>
-      <div>{`City: ${String(cityName)}`}</div>
-      <div>{`Temp: ${String(cityTemp)} *C`}</div>
-      <div>{`Pressure: ${String(cityPressure)} hPa`}</div>
-      <div>{`Wind: ${String(cityWind)} m/s`}</div>
-    </>
+    <div clasName="resultContainer">
+      {cityErr ? `We haven't ${cityName} in database.` : container}
+    </div>
   );
 }
 
