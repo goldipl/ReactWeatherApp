ca2811eaa2f6b8e03360737ed5a116cdefe4d548
goldipl <mgodlewski92@gmail.com>
2021-11-27 13:51:03 +0100

    add title and css

--- src/comp/SearchSection.js
+++ src/comp/SearchSection.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 const SearchSection = (props) => {
   return (
-    <form onSubmit={props.citySubmit}>
-      <input type="text"
-        value={props.value}
-        onChange={props.cityChange}
-        placeholder="Type City..."
-      />
-      <button>Search city</button>
-    </form>
+    <>
+      <h2>Weather App</h2>
+      <form onSubmit={props.citySubmit}>
+        <input type="text"
+          value={props.value}
+          onChange={props.cityChange}
+          placeholder="Type City..."
+        />
+        <button>Search city</button>
+      </form>
+    </>
   );
 }
 
