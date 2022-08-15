6bd420603ad77ca439e318955da3e3cd0a9d43e3
goldipl <mgodlewski92@gmail.com>
2022-08-15 11:36:38 +0200

    add new classes to resultContainer

--- src/comp/WeatherOutput.js
+++ src/comp/WeatherOutput.js
@@ -8,14 +8,12 @@ const WeatherOutput = (props) => {
 
   if (cityName && !cityErr) {
     container = (
-      <>
         <div className="resultContainer">
-          <div><strong>City: </strong>{String(cityName)}</div>
-          <div><strong>Temp: </strong>{`${String(cityTemp)}`} &#176;C</div>
-          <div><strong>Pressure: </strong>{`${String(cityPressure)} hPa`}</div>
-          <div><strong>Wind: </strong>{`${String(cityWind)} m/s`}</div>
+          <div className="city"><strong>City: </strong>{String(cityName)}</div>
+          <div className="temp"><strong>Temp: </strong>{`${String(cityTemp)}`} &#176;C</div>
+          <div className="pressure"><strong>Pressure: </strong>{`${String(cityPressure)} hPa`}</div>
+          <div className="wind"><strong>Wind: </strong>{`${String(cityWind)} m/s`}</div>
         </div>
-      </>
     )
   }
 
